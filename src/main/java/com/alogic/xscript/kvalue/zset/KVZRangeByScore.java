6e1c2ecf49b207daf73c54155c1764bd1bc52743
cenwan <732433736@qq.com>
2016-09-07 11:13:16 +0800

    KVZRangeByScore测试版

--- src/main/java/com/alogic/xscript/kvalue/zset/KVZRangeByScore.java
+++ src/main/java/com/alogic/xscript/kvalue/zset/KVZRangeByScore.java
@@ -13,10 +13,8 @@ import com.alogic.xscript.kvalue.KVRowOperation;
 import com.anysoft.util.Pair;
 import com.anysoft.util.Properties;
 import com.anysoft.util.PropertiesConstants;
-import com.logicbus.backend.ServantException;
 import com.logicbus.kvalue.core.KeyValueRow;
 import com.logicbus.kvalue.core.SortedSetRow;
-
 
 /**
  * @author zhongyi
@@ -29,7 +27,6 @@ public class KVZRangeByScore extends KVRowOperation {
 	protected String withscores = "false";
 	protected String reverse = "false";
 	protected String tag = "data";
-	
 	
 	/**
 	 * 预留2个参数用来支持分页
@@ -64,28 +61,35 @@ public class KVZRangeByScore extends KVRowOperation {
 				
 				boolean _reverse=getBoolean(ctx.transform(reverse), false);
 				List<Pair<String,Double>> l=null;
+			/*	if(_reverse){
+					current.put(ctx.transform(tag), r.rangeByScoreWithScores(getDouble(ctx.transform(max), 0), getDouble(ctx.transform(min), 150d),
+							_reverse, getLong(ctx.transform(offset), 0), getLong(ctx.transform(count), 100)));
+				}else{
+					current.put(ctx.transform(tag), r.rangeByScoreWithScores(getDouble(ctx.transform(min), 0), getDouble(ctx.transform(max), 150d),
+							_reverse, getLong(ctx.transform(offset), 0), getLong(ctx.transform(count), 100)));
+				}*/
 				if(_reverse){
 					l=r.rangeByScoreWithScores(getDouble(ctx.transform(max), 0), getDouble(ctx.transform(min), 150d),
-							_reverse);
+							_reverse,getLong(ctx.transform(offset), 0), getLong(ctx.transform(count), 100l));
 				}else{
 					l=r.rangeByScoreWithScores(getDouble(ctx.transform(min), 0), getDouble(ctx.transform(max), 150d),
-							_reverse);
+							_reverse,getLong(ctx.transform(offset), 0l), getLong(ctx.transform(count), 100l));
 				}
 			    
-//				List<Map<String,Double>> result=new ArrayList<Map<String,Double>>();
-//				if(null!=l&&l.size()>0){
-//					Iterator<Pair<String,Double>> ite=l.iterator();
-//					while(ite.hasNext()){
-//						Pair<String,Double> p=ite.next();
-//						Map<String,Double> map=new HashMap<String,Double>();
-//						map.put(p.key(), p.value());
-//						result.add(map);
-//					}
-//				}
-//				
-//				current.put(ctx.transform(tag), result);
+				List<Map<String,Double>> result=new ArrayList<Map<String,Double>>();
+				if(null!=l&&l.size()>0){
+					Iterator<Pair<String,Double>> ite=l.iterator();
+					while(ite.hasNext()){
+						Pair<String,Double> p=ite.next();
+						Map<String,Double> map=new HashMap<String,Double>();
+						map.put(p.key(), p.value());
+						result.add(map);
+					}
+				}
+				
+				current.put(ctx.transform(tag), result);
 
-				int offsetInt = getInt(ctx.transform(offset), 0);
+/*				int offsetInt = getInt(ctx.transform(offset), 0);
 				int countInt = getInt(ctx.transform(count), 100);
 				if(offsetInt >= 0 && offsetInt <= l.size()){
 					List<Map<String,Double>> res=new ArrayList<Map<String,Double>>();
@@ -99,18 +103,18 @@ public class KVZRangeByScore extends KVRowOperation {
 				}else if(offsetInt < 0){
 					throw new ServantException("core.failed", String.format(
 							"empty list or set,offset can not be a negative!offset:%s", offset));
-				}
+				}*/
 				
 			}else{
 				boolean _reverse=getBoolean(ctx.transform(reverse), false);
-				List<Pair<String,Double>> l=null;
+				//List<Pair<String,Double>> l=null;
 				if(_reverse){
 					current.put(ctx.transform(tag), r.rangeByScore(getDouble(ctx.transform(max), 0), getDouble(ctx.transform(min), 150l),
-							_reverse));
+							_reverse, getLong(ctx.transform(offset), 0), getLong(ctx.transform(count), 100)));
 				}else{
 					current.put(ctx.transform(tag), r.rangeByScore(getDouble(ctx.transform(min), 0), getDouble(ctx.transform(max), 150l),
-							_reverse));
-				}				
+							_reverse, getLong(ctx.transform(offset), 0), getLong(ctx.transform(count), 100)));
+				}
 			}			
 		}
 	}
